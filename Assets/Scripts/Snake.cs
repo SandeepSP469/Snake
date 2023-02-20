0a1358828e29de9b2138835342466b07fa4432d9
SandeepSP <119571804+SandeepSP469@users.noreply.github.com>
2023-02-20 15:52:47 +0530

    New

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Snake : MonoBehaviour
+{
+    private List<Transform> segments = new List<Transform>();
+    public Transform segmentPrefab;
+
+    public Vector2 direction = Vector2.right;
+    public int initialSize = 4;
+
+    public float speed = 20f;
+    public float speedMultiplier = 1f;
+
+    private float nextUpdate;
+
+    private void Start()
+    {
+        ResetState();
+    }
+
+    private void Update()
+    {
+        // Only allow turning up or down while moving in the x-axis
+        if (direction.x != 0f)
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+                direction = Vector2.up;
+            } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+                direction = Vector2.down;
+            }
+        }
+        // Only allow turning left or right while moving in the y-axis
+        else if (direction.y != 0f)
+        {
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+                direction = Vector2.right;
+            } else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+                direction = Vector2.left;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Wait until the next update before proceeding
+        if (Time.time < nextUpdate) {
+            return;
+        }
+
+        // Set each segment's position to be the same as the one it follows. We
+        // must do this in reverse order so the position is set to the previous
+        // position, otherwise they will all be stacked on top of each other.
+        for (int i = segments.Count - 1; i > 0; i--) {
+            segments[i].position = segments[i - 1].position;
+        }
+
+        // Move the snake in the direction it is facing
+        // Round the values to ensure it aligns to the grid
+        float x = Mathf.Round(transform.position.x) + direction.x;
+        float y = Mathf.Round(transform.position.y) + direction.y;
+
+        transform.position = new Vector2(x, y);
+        nextUpdate = Time.time + (1f / (speed * speedMultiplier));
+    }
+
+    public void Grow()
+    {
+        Transform segment = Instantiate(segmentPrefab);
+        segment.position = segments[segments.Count - 1].position;
+        segments.Add(segment);
+    }
+
+    public void ResetState()
+    {
+        direction = Vector2.right;
+        transform.position = Vector3.zero;
+
+        // Start at 1 to skip destroying the head
+        for (int i = 1; i < segments.Count; i++) {
+            Destroy(segments[i].gameObject);
+        }
+
+        // Clear the list but add back this as the head
+        segments.Clear();
+        segments.Add(transform);
+
+        // -1 since the head is already in the list
+        for (int i = 0; i < initialSize - 1; i++) {
+            Grow();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Food")) {
+            Grow();
+        } else if (other.gameObject.CompareTag("Obstacle")) {
+            ResetState();
+        }
+    }
+
+}
